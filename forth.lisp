9f7c74e6e34e50536e4b7e7db061e8d82bd49cac
Nathan Piel <nthnpiel@gmail.com>
2023-05-17 19:09:23 -0400

    Implemented more words

--- forth.lisp
+++ forth.lisp
@@ -34,7 +34,7 @@
      (progn (setf (word-code entry) ',body
                   (word-enabledp entry) ',enabledp
                   (word-immediatep entry) ',immediatep)
-            (format t "WARNING: REDEFINING '~a'" ',name))
+            (format t "WARNING: REDEFINING '~a'~%" ',name))
      (push (make-word :name ',name :immediatep ,immediatep :enabledp ,enabledp :code ',body) *dictionary*)))
 
 (defmacro with-scanner (&body body)
@@ -116,40 +116,119 @@
              (error "Cannot find '~a' in dictionary" word))))
 
 (defun forth-repl ()
-  (unwind-protect
-       (loop for line = (read-line)
-             until (string= line "bye")
-             do (with-scanner
-                  (setf source line
-                        start 0
-                        current 0)
-                  (interprete)
-                  (format t "~t~a~%" *stack*)))
-    (setf *compile-mode-p* nil
-          *locals* nil)
-    (vector-clear *stack*)
-    (vector-clear *memory*)))
+  (let ((*scanner* (make-scanner))
+        (*dictionary* *dictionary*)
+        (*locals* nil)
+        (*stack* (make-array 0 :element-type 'number :adjustable t :fill-pointer 0))
+        (*memory* (make-array 0 :element-type 'number :adjustable t :fill-pointer 0))
+        (*compile-mode-p* nil))
+    (loop for line = (read-line)
+          until (string= line "bye")
+          do (with-scanner
+               (setf source line
+                     start 0
+                     current 0)
+               (interprete)
+               (format t "~t~a~%" *stack*)))))
+
+(defword "lit" ()
+  (vector-push-extend (next-word)))
 
 (defword "+" ()
   (vector-push-extend (+ (vector-pop *stack*) (vector-pop *stack*)) *stack*))
+
+(defword "1+" ()
+  (incf (from-top *stack*)))
+
+(defword "4+" ()
+  (incf (from-top *stack*) 4))
 
 (defword "-" ()
   (let ((right (vector-pop *stack*))
         (left (vector-pop *stack*)))
     (vector-push-extend (- left right) *stack*)))
 
+(defword "1-" ()
+  (decf (from-top *stack*)))
+
+(defword "4-" ()
+  (decf (from-top *stack*) 4))
+
 (defword "*" ()
   (vector-push-extend (* (vector-pop *stack*) (vector-pop *stack*)) *stack*))
 
-(defword "/" ()
+(defword "/mod" ()
   (let ((denominator (vector-pop *stack*))
         (numerator (vector-pop *stack*)))
-    (vector-push-extend (/ numerator denominator) *stack*)))
+    (multiple-value-bind (quotient remainder) (floor numerator denominator)
+      (vector-push-extend remainder *stack*)
+      (vector-push-extend quotient *stack*))))
 
-(defword "mod" ()
-  (let ((divisor (vector-pop *stack*))
-        (number (vector-pop *stack*)))
-    (vector-push-extend (mod number divisor) *stack*)))
+(defword "=" ()
+    (let ((b (vector-pop *stack*))
+          (a (vector-pop *stack*)))
+      (vector-push-extend (if (= a b) 1 0) *stack*)))
+
+(defword "/=" ()
+    (let ((b (vector-pop *stack*))
+          (a (vector-pop *stack*)))
+      (vector-push-extend (if (/= a b) 1 0) *stack*)))
+
+(defword "0=" ()
+  (vector-push-extend (if (zerop (vector-pop *stack*)) 1 0) *stack*))
+
+(defword "0/=" ()
+  (vector-push-extend (if (not (zerop (vector-pop *stack*))) 1 0) *stack*))
+
+(defword "<" ()
+  (let ((b (vector-pop *stack*))
+        (a (vector-pop *stack*)))
+    (vector-push-extend (if (< a b) 1 0) *stack*)))
+
+(defword ">" ()
+    (let ((b (vector-pop *stack*))
+          (a (vector-pop *stack*)))
+      (vector-push-extend (if (> a b) 1 0) *stack*)))
+
+(defword "<=" ()
+    (let ((b (vector-pop *stack*))
+          (a (vector-pop *stack*)))
+      (vector-push-extend (if (<= a b) 1 0) *stack*)))
+
+(defword ">=" ()
+    (let ((b (vector-pop *stack*))
+          (a (vector-pop *stack*)))
+      (vector-push-extend (if (>= a b) 1 0) *stack*)))
+
+(defword "0<" ()
+  (vector-push-extend (if (< (vector-pop *stack*) 0) 1 0) *stack*))
+
+(defword "0>" ()
+  (vector-push-extend (if (> (vector-pop *stack*) 0) 1 0) *stack*))
+
+(defword "0<=" ()
+  (vector-push-extend (if (<= (vector-pop *stack*) 0) 1 0) *stack*))
+
+(defword "0>=" ()
+  (vector-push-extend (if (>= (vector-pop *stack*) 0) 1 0) *stack*))
+
+(defword "and" ()
+    (let ((b (vector-pop *stack*))
+          (a (vector-pop *stack*)))
+      (vector-push-extend (logand a b) *stack*)))
+
+(defword "or" ()
+    (let ((b (vector-pop *stack*))
+          (a (vector-pop *stack*)))
+      (vector-push-extend (logior a b) *stack*)))
+
+(defword "xor" ()
+    (let ((b (vector-pop *stack*))
+          (a (vector-pop *stack*)))
+      (vector-push-extend (logxor a b) *stack*)))
+
+(defword "not" ()
+  (vector-push-extend (lognot (vector-pop *stack*)) *stack*))
 
 (defword "swap" ()
   (rotatef (from-top *stack*) (from-top *stack* 1)))
@@ -157,12 +236,6 @@
 (defword "2swap" ()
   (rotatef (from-top *stack*) (from-top *stack* 2))
   (rotatef (from-top *stack* 1) (from-top *stack* 3)))
-
-(defword "/mod" ()
-  (let ((denominator (vector-pop *stack*)) (numerator (vector-pop *stack*)))
-    (multiple-value-bind (quotient remainder) (floor numerator denominator)
-      (vector-push-extend remainder *stack*)
-      (vector-push-extend quotient *stack*))))
 
 (defword "negate" ()
   (vector-push (- (vector-pop *stack*)) *stack*))
@@ -184,6 +257,16 @@
 (defword "dup" ()
   (vector-push-extend (from-top *stack*) *stack*))
 
+(defword "2dup" ()
+  (let ((a (from-top *stack*))
+        (b (from-top *stack* 1)))
+    (vector-push-extend b *stack*)
+    (vector-push-extend a *stack*)))
+
+(defword "?dup" ()
+  (unless (zerop (from-top *stack*))
+    (vector-push-extend (from-top *stack*) *stack*)))
+
 (defword "over" ()
   (vector-push-extend (from-top *stack* 1) *stack*))
 
@@ -193,6 +276,10 @@
 (defword "rot" ()
   (rotatef (from-top *stack*) (from-top *stack* 1))
   (rotatef (from-top *stack*) (from-top *stack* 2)))
+
+(defword "-rot" ()
+  (rotatef (from-top *stack*) (from-top *stack* 2))
+  (rotatef (from-top *stack*) (from-top *stack* 1)))
 
 (defword "\\" (:immediatep t)
   (with-scanner
@@ -247,6 +334,12 @@
 
 (defword "!" ()
   (setf (aref *memory* (vector-pop *stack*)) (vector-pop *stack*)))
+
+(defword "+!" ()
+  (incf (aref *memory* (vector-pop *stack*)) (vector-pop *stack*)))
+
+(defword "-!" ()
+  (decf (aref *memory* (vector-pop *stack*)) (vector-pop *stack*)))
 
 (defword "immediate" ()
   (setf (word-immediatep (first *dictionary*)) t))
